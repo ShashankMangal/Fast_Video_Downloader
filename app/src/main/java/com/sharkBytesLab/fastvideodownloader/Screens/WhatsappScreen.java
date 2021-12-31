a2c1e8a67fd4b84e567122ef03ea59a5d0fcc56a
ShashankMangal <shashankmangal10@gmail.com>
2021-12-31 18:15:54 +0530

    Main Activity Layout Ready

---
+++ app/src/main/java/com/sharkBytesLab/fastvideodownloader/Screens/WhatsappScreen.java
@@ -0,0 +1,16 @@
+package com.sharkBytesLab.fastvideodownloader.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sharkBytesLab.fastvideodownloader.R;
+
+public class WhatsappScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whatsapp_screen);
+    }
+}
